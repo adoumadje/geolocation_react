b979f6b65ccd5615d1b0169f6f58c9c09f0dad68
adoumadje <adoumadjecaleb@gmail.com>
2022-07-03 16:40:39 +0300

    fisrt and maybe last commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
+
+const API_endpoint = `https://api.openweathermap.org/data/2.5/onecall?`
+const API_key = `6a81e65b81c4fe907f5efc1c785ff400`
 
 function App() {
+  const [latitude, setLatitude] = React.useState()
+  const [longitude, setLongitude] = React.useState()
+
+  React.useEffect(() => {
+    navigator.geolocation.getCurrentPosition((position) => {
+        setLatitude(position.coords.latitude)
+        setLongitude(position.coords.longitude)
+    })
+
+    const finalAPIendpoint = `${API_endpoint}lat=${latitude}&lon=${longitude}&exclude=hourly,daily&appid=${API_key}`
+
+    axios.get(finalAPIendpoint)
+    .then((response) => {
+      console.log(response.data)
+    })
+  }, [latitude, longitude])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      App
     </div>
   );
 }
